0f811f444e777f1ea7e2dcf281dd5250cf93326c
juandroid007 <juanvillacortac@gmail.com>
2018-09-17 20:03:13 -0400

    Fixing last commit

---
+++ src/accelerators.cpp
@@ -0,0 +1,25 @@
+#include "accelerators.h"
+#include "kinematics.h"
+
+#include <algorithm>
+
+// Fall Motion
+const units::Acceleration kGravityAcceleration = 0.00078125f;
+const units::Velocity kTerminalSpeed = 0.2998046875f;
+
+const ZeroAccelerator ZeroAccelerator::kZero;
+const ConstantAccelerator ConstantAccelerator::kGravity(kGravityAcceleration, kTerminalSpeed);
+
+void ConstantAccelerator::updateVelocity(Kinematics& kinematics, units::MS elapsed_time) const {
+	if(acceleration_ < 0.0f) {
+		kinematics.velocity = std::max(kinematics.velocity + acceleration_ * elapsed_time, max_velocity_);
+	} else {
+		kinematics.velocity = std::min(kinematics.velocity + acceleration_ * elapsed_time, max_velocity_);
+	}
+}
+
+void FrictionAccelerator::updateVelocity(Kinematics& kinematics, units::MS elapsed_time) const {
+	kinematics.velocity = kinematics.velocity > 0.0f ?
+		std::max(0.0f, kinematics.velocity - friction_ * elapsed_time) :
+		std::min(0.0f, kinematics.velocity + friction_ * elapsed_time);
+}
