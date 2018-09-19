b5e318bd3ba23270b48c85f5b35a14719cbe5d0f
juandroid007 <juanvillacortac@gmail.com>
2018-09-19 17:47:58 -0400

    Fuck dat shit. Fixing last commit

---
+++ src/pickups.cpp
@@ -0,0 +1,36 @@
+#include "pickups.h"
+#include "pickup.h"
+#include "player.h"
+
+void Pickups::handleCollision(Player& player) {
+	for(PickupSet::iterator iter = pickups_.begin();
+			iter != pickups_.end();
+	   ) {
+		if(player.damageRectangle().collideWith((*iter)->collisionRectangle())) {
+			player.collectPickup(**iter);
+			pickups_.erase(iter++);
+		} else {
+			++iter;
+		}
+	}
+}
+
+void Pickups::update(units::MS elapsed_time, const Map& map) {
+	for(PickupSet::iterator iter = pickups_.begin();
+			iter != pickups_.end();
+	   ) {
+		if((*iter)->update(elapsed_time, map)) {
+			++iter;
+		} else {
+			pickups_.erase(iter++);
+		}
+	}
+}
+
+void Pickups::draw(Graphics& graphics) {
+	for(PickupSet::iterator iter = pickups_.begin();
+			iter != pickups_.end();
+			iter++) {
+		(*iter)->draw(graphics);
+	}
+} 
---
+++ src/pickups.h
@@ -0,0 +1,25 @@
+#ifndef PICKUPS_H_
+#define PICKUPS_H_
+
+#include "units.h"
+
+#include <memory>
+#include <set>
+
+struct Graphics;
+struct Map;
+struct Pickup;
+struct Player;
+
+class Pickups {
+	private:
+		typedef std::set<std::shared_ptr<Pickup> > PickupSet;
+		PickupSet pickups_;
+	public:
+		void add(std::shared_ptr<Pickup> pickup) { pickups_.insert(pickup); }
+		void handleCollision(Player& player);
+		void update(units::MS elapsed_time, const Map& map);
+		void draw(Graphics& graphics);
+};
+
+#endif // PICKUPS_H_ 
