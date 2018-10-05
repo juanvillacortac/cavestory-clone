5e47162622a49b4ed1458f9e02332d65fd089fa7
juandroid007 <juanvillacortac@gmail.com>
2018-10-04 21:48:37 -0400

    Now Quote only can fire when is not interacting

--- src/player.cpp
+++ src/player.cpp
@@ -288,7 +288,8 @@ void Player::stopJump() {
 }
 
 void Player::startFire() {
-	polar_star_.startFire(kinematics_x_.position, kinematics_y_.position, horizontal_facing_, vertical_facing(), gun_up(), particle_tools_);
+	if(!interacting_)
+		polar_star_.startFire(kinematics_x_.position, kinematics_y_.position, horizontal_facing_, vertical_facing(), gun_up(), particle_tools_);
 }
 void Player::stopFire() {
 	polar_star_.stopFire();
